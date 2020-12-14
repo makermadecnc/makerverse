5f11f5ae04c782ea0e3c7a046cc4beb7a8896ef9
Zane Claes <zaneclaes@gmail.com>
2020-12-14 11:31:27 -0700

    About to merge

--- Lib/Controllers/ApiRoute.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Makerverse.Lib {
-  public class ApiRouteAttribute : RouteAttribute {
-    public ApiRouteAttribute(string template) : base($"api/{template}") { }
-  }
-}
