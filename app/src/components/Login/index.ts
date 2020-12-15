53929e50732efa329837aa88ea45866169cf0055
Zane Claes <zaneclaes@gmail.com>
2020-12-15 15:09:05 -0700

    Hot app deployment

--- app/src/components/Login/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as LoginPage } from './LoginPage';
-export { default as CallbackPage } from './CallbackPage';
--- app/src/constants/docs.ts
+++
@@ -1,6 +0,0 @@
-const root = 'http://www.makerverse.com';
-
-export default {
-  urlSecurity: `${root}/features/security`,
-  urlAbout: `${root}/about`,
-};
