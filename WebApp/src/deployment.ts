22ae393ade6abfe8c4e800774672031a5eb70d67
Zane Claes <zane@claes.biz>
2021-12-14 10:57:00 -0700

    New brand/style

--- WebApp/src/deployment.ts
+++ WebApp/src/deployment.ts
@@ -3,7 +3,7 @@ import {
   prepareDeployment,
 } from '@openworkshop/maker-hub/deployments';
 import deploymentJson from './deployment.json';
-import theme from './MakerverseTheme';
+import theme from '@openworkshop/maker-hub/themes/MakerverseTheme';
 
 console.log('[PACKAGE]', deploymentJson.package);
 const deployment = prepareDeployment(deploymentJson as IMakerHubDeploymentData);
